d990d877c66f41cd866deab0c72da5c729be845b
Tetsuo Asato <otetsu@gmail.com>
2016-02-18 14:09:23 +0900

    Kadai1kuku

--- src/Kuku.java
+++ src/Kuku.java
@@ -4,72 +4,50 @@ import java.io.InputStreamReader;
 public class Kuku {
 
 	public static void main(String[] args) {
-		String str = null;
-		String str2 = null;
-		int x = 0;
-		int y = 0;
-		boolean flg = true;
 
-		while (flg) {
-			try {
-				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-				System.out.println("xの値をいれてください。 >\n");
 
-				str = br.readLine();
-				x = Integer.parseInt(str);
 
-				if (isNumber(str)) {
-					if (x >= 1 && x <= 9) {
-						break;
-					} else {
-						System.out.println("1～9で入力してください\n");
-						continue;
-					}
-				} else {
-					System.out.println("数字で入力してください\n");
-					continue;
-				}
+		        int x = 0;
+		        int y = 0;
+		        String str = null;
+		        try {
+		            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		            System.out.println("九九表 \n縦列の値を入力してね");
 
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+		            str = br.readLine();
+		            x = Integer.parseInt(str);
+		            System.out.print("横列の値を入力して下さい");
+		            str = br.readLine();
+		            y = Integer.parseInt(str);
+
+		            for (int i = 1; i <= 100; i++) {
+
+		                for (int j = 1; j <= 100; j++) {
+		                    if (i <= x) {
+		                        if (j <= y) {
+		                            System.out.print((i * j) + " ");
+		                        }
+		                    }
+		                }
+		                System.out.println("");
+		                if (isNumber(str)) {
+		                    System.out.print("次の数値を入力して下さい");
+		                    break;
+		                } else if (x == 0 && y == 0) {
+		                    System.out.print("もう一度入力して下さい");
+		                    continue;
+		                }
+		            }
+
+		        } catch (Exception e) {
+		            System.out.println("入力エラー\n 最初からやり直し");
+		        }
+		    }
+
+		    private static boolean isNumber(String str) {
+		        return false;
+		    }
 		}
 
-
-
-
-	while (flg) {
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			System.out.println("yの値を入力してください > \n" );
-			str2 = br.readLine();
-			y = Integer.parseInt(str2);
-
-			if (isNumber(str2)) {
-				if (y >= 1 && y <= 9) {
-					break;
-				} else {
-					System.out.println("1～9で入力してください\n");
-					continue;
-				}
-			} else {
-				System.out.println("数字で入力してください\n");
-				continue;
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-	System.out.println(x*y);
-}
-
-	private static boolean isNumber(String str) {
-		// TODO 自動生成されたメソッド・スタブ
-		return false;
-	}
-
-	}
 
 
